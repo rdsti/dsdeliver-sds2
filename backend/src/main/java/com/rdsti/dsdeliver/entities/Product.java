865065103536d644fa1b2cd3fe770e7273930a3f
Rodrigo Siqueira <rodrigo.siqueira@rdsti.com.br>
2021-01-05 07:58:55 -0300

    Domain model

---
+++ backend/src/main/java/com/rdsti/dsdeliver/entities/Product.java
@@ -0,0 +1,155 @@
+/**
+ * 
+ */
+package com.rdsti.dsdeliver.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @business RDS - TI
+ * @author rodrigo.siqueira
+ * @system dsdeliver
+ * @create 5 de jan. de 2021
+ * @version 1.0
+ */
+
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Double price;
+	private String description;
+	private String imageUri;
+	
+	public Product() {
+
+	}
+	
+	/**
+	 * @param id
+	 * @param name
+	 * @param price
+	 * @param description
+	 * @param imageUri
+	 */
+	public Product(Long id, String name, Double price, String description, String imageUri) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.description = description;
+		this.imageUri = imageUri;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public Double getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the imageUri
+	 */
+	public String getImageUri() {
+		return imageUri;
+	}
+
+	/**
+	 * @param imageUri the imageUri to set
+	 */
+	public void setImageUri(String imageUri) {
+		this.imageUri = imageUri;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+}
---
+++ backend/src/main/java/com/rdsti/dsdeliver/enuns/OrderStatus.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.rdsti.dsdeliver.enuns;
+
+/**
+ * @business RDS - TI
+ * @author rodrigo.siqueira
+ * @system dsdeliver
+ * @create 5 de jan. de 2021
+ * @version 1.0
+ */
+
+public enum OrderStatus {
+	
+	PENDING, DELIVERED;
+
+}
