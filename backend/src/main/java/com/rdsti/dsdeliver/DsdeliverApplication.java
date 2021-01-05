d30ec006123954fe38bc5669ccc4e413d7c5da6e
Rodrigo Siqueira <rodrigo.siqueira@rdsti.com.br>
2021-01-05 07:18:26 -0300

    Initial Setup

--- backend/src/main/java/com/rdsti/dsdeliver/DsdeliverApplication.java
+++ backend/src/main/java/com/rdsti/dsdeliver/DsdeliverApplication.java
@@ -3,6 +3,14 @@ package com.rdsti.dsdeliver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @business RDS - TI
+ * @author rodrigo.siqueira
+ * @system dsdeliver
+ * @create 5 de jan. de 2021
+ * @version 1.0
+ */
+
 @SpringBootApplication
 public class DsdeliverApplication {
 
