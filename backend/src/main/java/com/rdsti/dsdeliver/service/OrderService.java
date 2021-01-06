88c53cd5d0078fa36993695197c40163997398e5
Rodrigo Siqueira <rodrigo.siqueira@rdsti.com.br>
2021-01-06 11:10:13 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/com/rdsti/dsdeliver/service/OrderService.java
+++ backend/src/main/java/com/rdsti/dsdeliver/service/OrderService.java
@@ -56,7 +56,17 @@ public class OrderService {
 			
 		}
 		
-		order = repository.saveAndFlush(order);
+		order = repository.save(order);
+		
+		return new OrderDTO(order);
+	}
+	
+	@Transactional
+	public OrderDTO setDelivered(Long id){
+		
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = repository.save(order);
 		
 		return new OrderDTO(order);
 	}
